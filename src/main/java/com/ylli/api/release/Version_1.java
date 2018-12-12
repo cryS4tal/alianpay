386e4e2b68e0ca6c5197329ccb4fe0ab3efedb4c
cryStal <ylli@tmindtech.com>
2018-12-12 16:35:41 +0800

    feature: temp code.todo test

--- src/main/java/com/ylli/api/release/Version_1.java
+++ src/main/java/com/ylli/api/release/Version_1.java
@@ -2,6 +2,8 @@ package com.ylli.api.release;
 
 import com.ylli.api.auth.mapper.AccountMapper;
 import com.ylli.api.auth.model.Account;
+import com.ylli.api.pay.mapper.BillMapper;
+import com.ylli.api.pay.model.Bill;
 import com.ylli.api.user.mapper.UserSettlementMapper;
 import com.ylli.api.user.model.UserSettlement;
 import com.ylli.api.wallet.mapper.CashLogMapper;
@@ -9,6 +11,8 @@ import com.ylli.api.wallet.mapper.WalletMapper;
 import com.ylli.api.wallet.model.CashLog;
 import com.ylli.api.wallet.model.Wallet;
 import com.ylli.api.wallet.service.WalletService;
+import com.ylli.api.yfbpay.mapper.YfbBillMapper;
+import com.ylli.api.yfbpay.model.YfbBill;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,10 +39,17 @@ public class Version_1 {
     @Autowired
     UserSettlementMapper userSettlementMapper;
 
+    @Autowired
+    YfbBillMapper yfbBillMapper;
+
+    @Autowired
+    BillMapper billMapper;
+
     @PostConstruct
     void init() {
         fixWallet();
         fullCashWithWallet();
+        fixBill();
     }
 
     /**
@@ -72,6 +83,44 @@ public class Version_1 {
             item.openBank = settlement.openBank;
             item.subBank = settlement.subBank;
             cashLogMapper.updateByPrimaryKeySelective(item);
+        });
+    }
+
+    /**
+     * 将原先账单 t_yfb_bill 数据迁移至 t_bill
+     */
+    void fixBill() {
+        YfbBill yfbBill = new YfbBill();
+        yfbBill.status = YfbBill.FINISH;
+        List<YfbBill> list = yfbBillMapper.select(yfbBill);
+        list.stream().forEach(item -> {
+            Bill exist = new Bill();
+            exist.sysOrderId = item.orderNo;
+            exist = billMapper.selectOne(exist);
+            if (exist != null) {
+                return;
+            }
+            Bill bill = new Bill();
+            bill.mchId = item.userId;
+            bill.sysOrderId = item.orderNo;
+            bill.mchOrderId = item.subNo;
+            bill.superOrderId = item.superNo;
+            bill.appId = 0l;
+            bill.channelId = 1l;
+            bill.money = item.amount;
+            bill.status = item.status;
+            bill.reserve = item.memo;
+            bill.notifyUrl = item.notifyUrl;
+            bill.redirectUrl = item.redirectUrl;
+            bill.msg = item.msg;
+            bill.isSuccess = item.isSuccess;
+            bill.payType = item.payType;
+            bill.tradeType = item.tradeType;
+            bill.tradeTime = item.tradeTime;
+            bill.payCharge = item.bonusMoney.intValue();
+            bill.createTime = item.createTime;
+            bill.modifyTime = item.modifyTime;
+            billMapper.insertSelective(bill);
         });
     }
 }
