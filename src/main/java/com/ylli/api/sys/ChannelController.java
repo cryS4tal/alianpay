a4dd7d29c62a9b95af727667ce33af3bb6db9f0e
cryStal <ylli@tmindtech.com>
2019-01-10 11:23:38 +0800

    feature: add 1024 test mch change channel by self.

--- src/main/java/com/ylli/api/sys/ChannelController.java
+++ src/main/java/com/ylli/api/sys/ChannelController.java
@@ -3,9 +3,12 @@ package com.ylli.api.sys;
 import com.ylli.api.base.annotation.Auth;
 import com.ylli.api.base.annotation.AwesomeParam;
 import com.ylli.api.base.annotation.Permission;
+import com.ylli.api.base.auth.AuthSession;
+import com.ylli.api.base.exception.AwesomeException;
 import com.ylli.api.sys.service.ChannelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/channel")
-@Auth(@Permission(Config.SysPermission.MANAGE_CHANNEL))
 public class ChannelController {
 
     @Autowired
     ChannelService channelService;
+
+    @Autowired
+    AuthSession authSession;
 
     static class Channel {
         public Long id;
@@ -34,11 +39,13 @@ public class ChannelController {
      * @param channel
      */
     @PostMapping("/sys")
+    @Auth(@Permission(Config.SysPermission.MANAGE_CHANNEL))
     public void channelSwitch(@RequestBody Channel channel) {
         channelService.channelSwitch(channel.id, channel.isOpen);
     }
 
     @GetMapping("/sys")
+    @Auth(@Permission(Config.SysPermission.MANAGE_CHANNEL))
     public Object sysChannels(@AwesomeParam(defaultValue = "0") int offset,
                               @AwesomeParam(defaultValue = "20") int limit) {
         return channelService.sysChannels(offset, limit);
@@ -55,8 +62,17 @@ public class ChannelController {
      * @param mchChannel
      */
     @PostMapping("/mch")
+    @Auth(@Permission(Config.SysPermission.MANAGE_CHANNEL))
     public void mchChannelSwitch(@RequestBody MchChannel mchChannel) {
         channelService.mchChannelSwitch(mchChannel.mchId, mchChannel.channelId);
     }
 
+    @PostMapping("/mch/{id}")
+    @Auth
+    public void changeChannel(@PathVariable Long id) {
+        if (1024 != authSession.getAuthId()) {
+            throw new AwesomeException(Config.ERROR_PERMISSION_DENY);
+        }
+        channelService.mchChannelSwitch(authSession.getAuthId(), id);
+    }
 }
